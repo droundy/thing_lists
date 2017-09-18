d6cd754b027b2b3ca23fa4cd626362fcf12039cc
David Roundy <roundyd@physics.oregonstate.edu>
2017-09-18 06:45:53 -0700

    add nice colors and all

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 
+import 'package:flutter_color_picker/flutter_color_picker.dart';
+
 import 'package:share/share.dart';
 
 final GoogleSignIn _googleSignIn = new GoogleSignIn();
@@ -78,6 +80,7 @@ class _ListPageState extends State<ListPage> {
   DatabaseReference _ref;
   List<String> _items = [];
   Map _keys = {};
+  Map _colors = {};
   final GlobalKey<AnimatedListState> listKey = new GlobalKey<AnimatedListState>();
 
   _ListPageState({this.listname}) {
@@ -94,12 +97,23 @@ class _ListPageState extends State<ListPage> {
           });
           things.sort((a,b) => a['chosen'].compareTo(b['chosen']));
           things.forEach((thing) {
-                _items.add(thing['name']);
-                _keys[thing['name']] = new ValueKey(thing);
+                String t = thing['name'];
+                _items.add(t);
+                _keys[t] = new ValueKey(thing);
+                if (thing.containsKey('color')) {
+                  _colors[t] = new Color(thing['color']);
+                }
               });
         }
       });
     });
+  }
+
+  Color _color(String i) {
+    if (_colors.containsKey(i)) {
+      return _colors[i];
+    }
+    return const Color(0xffffffff);
   }
 
   @override
@@ -112,13 +126,60 @@ class _ListPageState extends State<ListPage> {
   Widget build(BuildContext context) {
     List<Widget> xx = [];
     _items.forEach((i) {
-          print('one item is $i');
+          Widget menu = new PopupMenuButton<String>(
+              child: const Icon(Icons.more_vert),
+              itemBuilder: (BuildContext context) => [
+                new PopupMenuItem<String>(
+                    value: 'color',
+                    child: const Text('color')),
+                new PopupMenuItem<String>(
+                    value: 'rename',
+                    child: const Text('rename')),
+              ],
+              onSelected: (selected) async {
+                if (selected == 'color') {
+                  Color color = await showDialog(
+                      context: context,
+                      child: new PrimaryColorPickerDialog());
+                  if (color != null) {
+                    print('color: ${color.value}');
+                    await _ref.child(i).child('color').set(color.value);
+                  }
+                } else if (selected == 'rename') {
+                  String newname = await textInputDialog(context,
+                      'Rename $listname thing?', i);
+                  if (newname != null && newname != i) {
+                    Map data = (await _ref.child(i).once()).value;
+                    _ref.child(newname).set(data);
+                    _ref.child(i).remove();
+                    DataSnapshot xx = (await _root.child(i).once());
+                    if (xx != null && xx.value != null) {
+                      _root.child(newname).set(xx.value);
+                      _root.child(i).remove();
+                    }
+                  }
+                }
+              });
+
           xx.add(new Dismissible(
                   child: new Hero(key: new ValueKey(i), child: new Card(
+                      color: _color(i),
                       child: new ListTile(
                           title: new Text(i),
-                          leading: new Icon(Icons.more_vert),
-                          trailing: new Icon(Icons.delete),
+                          leading: menu, // const Icon(Icons.more_vert),
+                          // trailing: const Icon(Icons.delete),
+                          trailing: new IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () async {
+                                final bool confirmed = await confirmDialog(context,
+                                    'Really delete $i?', 'DELETE');
+                                print('confirmed is $confirmed');
+                                if (confirmed) {
+                                  print('am deleting $i');
+                                  _ref.child(i).remove();
+                                  _root.child(i).remove();
+                                }
+                              }),
                           onLongPress: () async {
                             print('selected $i');
                             Navigator.of(context).pushNamed('/$i');
@@ -154,18 +215,20 @@ class _ListPageState extends State<ListPage> {
             children: xx),
         floatingActionButton: new FloatingActionButton(
             onPressed: () async {
-              String newitem = await textInputDialog(context, 'New $listname?');
-              Map data = {};
-              DataSnapshot old = await _ref.once();
-              if (old.value != null) {
-                data = old.value;
+              String newitem = await textInputDialog(context, 'New $listname thing?');
+              if (newitem != null) {
+                Map data = {};
+                DataSnapshot old = await _ref.once();
+                if (old.value != null) {
+                  data = old.value;
+                }
+                data[newitem] = {
+                  'chosen': new DateTime.now().millisecondsSinceEpoch,
+                  'ignored': 0,
+                };
+                _ref.set(data);
+                print('got $newitem');
               }
-              data[newitem] = {
-                'chosen': new DateTime.now().millisecondsSinceEpoch,
-                'ignored': 0,
-              };
-              _ref.set(data);
-              print('got $newitem');
             },
             tooltip: 'Increment',
             child: new Icon(Icons.add),
@@ -181,11 +244,12 @@ void main() {
           }));
 }
 
-Future<String> textInputDialog(BuildContext context, String title) async {
+Future<String> textInputDialog(BuildContext context, String title, [String value]) async {
   String foo;
   return showDialog(context: context,
       child: new AlertDialog(title: new Text(title),
           content: new TextField(
+              controller: new TextEditingController(text: value),
               onChanged: (String newval) {
             foo = newval;
           },
@@ -207,4 +271,21 @@ Future<String> textInputDialog(BuildContext context, String title) async {
                            ),
           ]),
                     );
+}
+
+Future<bool> confirmDialog(BuildContext context, String title, String action) async {
+  return showDialog(context: context,
+      child: new AlertDialog(title: new Text(title),
+          actions: <Widget>[
+            new FlatButton(
+                child: new Text('CANCEL'),
+                onPressed: () {
+              Navigator.pop(context, false);
+            }),
+            new FlatButton(
+                child: new Text(action),
+                onPressed: () {
+              Navigator.pop(context, true);
+            }),
+          ]));
 }
